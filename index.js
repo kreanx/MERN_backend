c82cb12f02db9c5353315b92b84ae19f927f1cad
Nikita <oficialsmashgame@gmail.com>
2022-11-25 14:47:46 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,57 @@
+import express from 'express'
+import mongoose from 'mongoose'
+import { registerValidation, loginValidation, postCreateValidation } from './validations.js'
+import multer from 'multer'
+
+import {handleValidationErrors, checkAuth} from './utils/index.js'
+
+import { UserController, PostController } from './controllers/index.js'
+
+import cors from 'cors'
+
+mongoose
+    .connect('mongodb+srv://Admin:12345@cluster0.m8yzrbr.mongodb.net/blog?retryWrites=true&w=majority')
+    .then(() => console.log('DB ok'))
+    .catch((error) => console.log('DB error:', error))
+
+const app = express()
+
+const storage = multer.diskStorage({
+    destination: (_, __, cb) => {
+        cb(null, 'uploads')
+    },
+    filename: (_, file, cb) => {
+        cb(null, file.originalname)
+    },
+})
+
+const upload = multer({ storage })
+
+app.use(express.json())
+app.use(cors())
+app.use('/uploads', express.static('uploads'))
+
+app.post('/auth/login', loginValidation, handleValidationErrors, UserController.login)
+app.post('/auth/register', registerValidation, handleValidationErrors, UserController.register)
+app.get('/auth/me', checkAuth, UserController.getMe)
+
+app.post('/upload', checkAuth, upload.single('image'), (req, res) => {
+    res.json({
+        url: `/uploads/${req.file.originalname}`,
+    })
+})
+
+app.get('/posts', PostController.getAll)
+app.get('/tags', PostController.getLastTags)
+app.get('/posts/tags', PostController.getLastTags)
+app.get('/posts/:id', PostController.getOne)
+app.post('/posts', checkAuth, postCreateValidation, handleValidationErrors, PostController.create)
+app.delete('/posts/:id', checkAuth, PostController.remove)
+app.patch('/posts/:id', checkAuth, postCreateValidation, handleValidationErrors, PostController.update)
+
+app.listen(4444, (err) => {
+    if (err) {
+        return console.log('Error!')
+    }
+    console.log('Server OK')
+})
